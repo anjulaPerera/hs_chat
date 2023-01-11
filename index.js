ad03851c234e100c427c2c01d95d5bfdd726c450
Anjula Perera <anjulaperera98@gmail.com>
2023-01-11 09:40:27 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+var http = require("http");
+// const cors = require("cors")
+const app = express();
+
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+app.use(express.json());
+// app.use(cors())
+
+io.on("connection",(socket)=>{
+    console.log("connected");
+    console.log(socket.id,"has joined");
+    socket.on("/test",(msg)=>console.log(msg));
+});
+
+server.listen(port,"0.0.0.0",()=>{
+    console.log("server started",port);
+});
+
